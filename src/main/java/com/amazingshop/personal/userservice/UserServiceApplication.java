7952f12d1d4d8698a9417529144fbd6dc82a1905
Matvey Shavlov <m.shavloff@gmail.com>
2026-01-19 09:50:20 +0300

    chore: add Redis dependencies and configuration

--- src/main/java/com/amazingshop/personal/userservice/UserServiceApplication.java
+++ src/main/java/com/amazingshop/personal/userservice/UserServiceApplication.java
@@ -1,10 +1,10 @@
 package com.amazingshop.personal.userservice;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class UserServiceApplication {
 
@@ -12,8 +12,4 @@ public class UserServiceApplication {
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
-	@Bean
-	public ModelMapper modelMapper() {
-		return new ModelMapper();
-	}
 }
